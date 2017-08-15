1d7c3c183b1e3da045522b9d5267888738994a08
root <root@oob-mgmt-server>
2017-08-15 09:19:04 +0000

    pcap

--- setup_flowd.sh
+++ setup_flowd.sh
@@ -4,21 +4,23 @@ curl -O https://storage.googleapis.com/google-code-archive-downloads/v2/code.goo
 
 echo
 
-echo Installing byacc from apt-get (dependency)
+echo "Installing byacc from apt-get (dependency)"
 sudo apt-get update -y
 sudo apt-get install byacc -y
 
 echo Expanding flowd source
 tar xvfz flowd-0.9.1.tar.gz
 
-echo Next steps
-echo Build and install flowd
+echo
+echo NEXT STEPS
+echo 1. Build and install flowd
 echo cd into flowd-0.9.1
 echo configure
 echo make
 echo sudo make install 
-
 echo
-echo Build and install python flowd library (written in C)
+echo
+echo "2. Build and install python flowd library (written in C)"
 echo python setup.py build 
+echo # The above command may print cosmetic errors
 echo sudo python setup.py install
--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python 
 
 print("testing python import of flowd")
+
 import flowd
+
+flow_log = flowd.FlowLog("flows.log", "rb")
+for flow in flow_log:
+    print flow.format()
